a2f174718fe2f5821b98dcde07775891cb24084e
Thoren <thoren.paulson@gmail.com>
2022-04-17 15:45:38 -0700

    Add benchmarks.

---
+++ benches/overhead.rs
@@ -0,0 +1,87 @@
+use criterion::{criterion_group, criterion_main, Criterion, black_box};
+use tracing::{info, instrument};
+use tracing_subscriber::prelude::*;
+
+#[instrument]
+fn fibonacci(n: u64) -> u64 {
+    if n < 2 {
+        n
+    } else {
+        fibonacci(n - 1) + fibonacci(n - 2)
+    }
+}
+
+fn instrument_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("instrument");
+    group.throughput(criterion::Throughput::Elements(3));
+
+    group.bench_function("locations_and_args", |b| {
+        let (layer, _guard) = tracing_chrome::ChromeLayerBuilder::new()
+            .include_args(true)
+            .include_locations(true)
+            .build();
+        let _subscriber = tracing_subscriber::registry().with(layer).set_default();
+        b.iter(|| {
+            black_box(fibonacci(3))
+        });
+    });
+    group.bench_function("locations", |b| {
+        let (layer, _guard) = tracing_chrome::ChromeLayerBuilder::new()
+            .include_args(false)
+            .include_locations(true)
+            .build();
+        let _subscriber = tracing_subscriber::registry().with(layer).set_default();
+        b.iter(|| {
+            black_box(fibonacci(3))
+        });
+    });
+    group.bench_function("minimal", |b| {
+        let (layer, _guard) = tracing_chrome::ChromeLayerBuilder::new()
+            .include_args(false)
+            .include_locations(false)
+            .build();
+        let _subscriber = tracing_subscriber::registry().with(layer).set_default();
+        b.iter(|| {
+            black_box(fibonacci(3))
+        });
+    });
+}
+
+fn event_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("event");
+    group.throughput(criterion::Throughput::Elements(1));
+
+    group.bench_function("locations_and_args", |b| {
+        let (layer, _guard) = tracing_chrome::ChromeLayerBuilder::new()
+            .include_args(true)
+            .include_locations(true)
+            .build();
+        let _subscriber = tracing_subscriber::registry().with(layer).set_default();
+        b.iter(|| {
+            black_box(info!(arg = 42, "Something Happen"));
+        });
+    });
+    group.bench_function("locations", |b| {
+        let (layer, _guard) = tracing_chrome::ChromeLayerBuilder::new()
+            .include_args(false)
+            .include_locations(true)
+            .build();
+        let _subscriber = tracing_subscriber::registry().with(layer).set_default();
+        b.iter(|| {
+            black_box(info!(arg = 42, "Something Happen"));
+        });
+    });
+    group.bench_function("minimal", |b| {
+        let (layer, _guard) = tracing_chrome::ChromeLayerBuilder::new()
+            .include_args(false)
+            .include_locations(false)
+            .build();
+        let _subscriber = tracing_subscriber::registry().with(layer).set_default();
+        b.iter(|| {
+            black_box(info!(arg = 42, "Something Happen"));
+        });
+    });
+}
+
+criterion_group!(benches, instrument_benchmark, event_benchmark);
+criterion_main!(benches);
